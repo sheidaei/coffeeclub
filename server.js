17390f2c3cccbe5d6bf8ee0d5d250113afd4db4b
Shahin Sheidaei <Shahin@Sheidaei.com>
2016-11-18 13:31:56 -0500

    dire attempt to get the mail function working

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 3000
+var express=require('express');
+var nodemailer = require("nodemailer");
+var app=express();
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "shahindirect",
+        pass: "directshah"
+    }
+});
+/*------------------SMTP Over-----------------------------*/
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+/*------------------Routing Started ------------------------*/
+
+app.get('/',function(req,res){
+    res.sendfile('index.html');
+});
+app.get('/send',function(req,res){
+    var mailOptions={
+        to : req.query.to,
+        subject : req.query.subject,
+        text : req.query.text
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
+
+/*--------------------Routing Over----------------------------*/
 
 app.listen(port, function () {
   console.log(`Server listening on port ${port}!`)
-})
+});
